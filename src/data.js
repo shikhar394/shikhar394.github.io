c553545f8e4c7a3c9f4b106b32f6a75c22164226
Shikhar Sakhuja <ss9131@nyu.edu>
2023-07-18 19:02:18 -0400

    pls

---
+++ src/data.js
@@ -0,0 +1,140 @@
+// Social Icon
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import EmailIcon from '@mui/icons-material/Email';
+import MoodIcon from '@mui/icons-material/Mood';
+
+// Dark Metal color combination
+var data = {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Shikhar Sakhuja",
+  headerTagline: [
+    //Line 1 For Header
+    // "Building digital",
+    // //Line 2 For Header
+    // "products, brands,",
+    // //Line 3 For Header
+    // "and experience",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+
+  //Contact Email
+  contactEmail: "shikharsakhuja@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  // projects: [
+
+  //   /*
+  //   If You Want To Add More Project just Copy and Paste This At The End
+  //   ,{
+  //       title: 'Project Five',
+  //       para: 'Something Amazing',
+  //       imageSrc: "",
+  //       url: ''
+  //   }
+  //   */
+  // ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+//   aboutParaOne:
+//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+//   aboutParaTwo:
+//     "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+//   aboutParaThree:
+//     "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+//   aboutImage:
+//     "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+//   skills: [
+//     {
+//       img: htmlIcon,
+//       para:
+//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+//     },
+//     {
+//       img: cssIcon,
+//       para:
+//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+//     },
+//     {
+//       img: jsIcon,
+//       para:
+//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+//     },
+//     {
+//       img: reactIcon,
+//       para:
+//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+//     },
+//     {
+//       img: designIcon,
+//       para:
+//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+//     },
+//     {
+//       img: codeIcon,
+//       para:
+//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+//     },
+//   ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+//   promotionHeading: "Heading",
+//   promotionPara:
+//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+//   // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { icon: GitHubIcon, 
+      url: "https://github.com/shikhar394",
+      fill: "black",
+    },
+    {
+      icon: TwitterIcon,
+      url: "https://www.twitter.com/shikhar394",
+      fill: "black",
+    },
+    {
+      icon: LinkedInIcon,
+      url: "https://linkedin.com/in/shikhar394",
+      fill: "black",
+    },
+    {
+      icon: EmailIcon,
+      url: "mailto: shikharsakhuja@gmail.com",
+      fill: "black",
+    },
+    {
+      icon: MoodIcon,
+      url: "https://bloom-flannel-f6b.notion.site/Top-100-e685d279c1e5428cb278f44390fd7292",
+      fill: "Black",
+    }
+  ],
+
+  // End Contact Section ---------------
+}
+
+export default data
--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Fade } from "react-awesome-reveal";
+import data from './data.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="container">
+      <div className="header-wrapper">
+        <Fade top>
+          <h2>
+            سلام | नमस्ते | Hello | 你好 | ਕਿਦਾਨ
+          </h2>
+        </Fade>
+        <Fade bottom>
+          <h2>
+            I'm Shikhar Sakhuja.
+          </h2>
+        </Fade>
+        <Fade bottom cascade>
+          <div className="heading-wrapper">
+            <h3>
+              Building <a href="https://www.teamsinta.com/" target="_blank" rel="noopener noreferrer">SintaHR</a> and fighting financial fraud at <a href="https://discord.com/" target="_blank" rel="noopener noreferrer">Discord</a>.
+            </h3>
+          </div>
+        </Fade>
+        <Fade bottom>
+          <div className="social-icons">
+            {data.social.map((socialLink, index) => {
+              const IconComponent = socialLink.icon;
+              return (
+                <a
+                  key={index}
+                  href={socialLink.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <IconComponent style={{ color: "#D6D6D6", fontSize: 40 }} />
+                </a>
+              );
+            })}
+          </div>
+        </Fade>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
